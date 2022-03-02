a67d2b2f7ff92dc6c79b4b6ff15e0f6588ee417e
Reynoso-Cortes-Oscar <oreynosoc1800lumno.ipn.mx>
2022-03-01 19:49:57 -0600

    first commit

---
+++ app/src/main/java/com/example/metroseguro/activiti1.java
@@ -0,0 +1,14 @@
+package com.example.metroseguro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activiti1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activiti1);
+    }
+}
